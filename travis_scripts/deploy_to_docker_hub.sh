76467d14fa6b0464ad41737faa4894fe5df70b6c
Daniel <github@tirzuman.com>
2018-11-07 23:08:55 +0200

    - travis ci build trigger #3

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Pushing service docker images to docker hub...."
+echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push gohelpfund/ghf-authentication-service:$BUILD_NAME
 docker push gohelpfund/ghf-campaign-service:$BUILD_NAME
