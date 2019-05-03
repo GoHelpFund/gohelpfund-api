36bab8d7acc416e5a21f73160bb81939b499892a
Daniel Tirzuman <github@tirzuman.com>
2019-05-04 00:41:54 +0300

    - added the donation service - refactored all modules due to naming conflicts

--- campaign-service/src/main/java/com/gohelpfund/api/v1/categories/controllers/exceptions/CategoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gohelpfund.api.v1.categories.controllers.exceptions;
-
-public class CategoryNotFoundException extends RuntimeException {
-
-    public CategoryNotFoundException(String id) {
-        super("Could not find category " + id);
-    }
-}
--- fundraiser-service/src/main/java/com/gohelpfund/api/v1/fundraisers/controllers/errors/ApiSubError.java
+++
@@ -1,5 +0,0 @@
-package com.gohelpfund.api.v1.fundraisers.controllers.errors;
-
-public abstract class ApiSubError {
-
-}
