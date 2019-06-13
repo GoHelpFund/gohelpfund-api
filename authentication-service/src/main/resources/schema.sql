f5636fa5eb68441eaca723d1b81563b845c4aa36
Daniel Tirzuman <github@tirzuman.com>
2019-06-13 09:43:29 +0300

    - race condition fix

--- authentication-service/src/main/resources/schema.sql
+++ authentication-service/src/main/resources/schema.sql
@@ -42,7 +42,7 @@ CREATE TABLE oauth_client_details (
 CREATE TABLE oauth_client_token (
   token_id                VARCHAR(255),
   token                   bytea,
-  authentication_id       VARCHAR(255),
+  authentication_id       VARCHAR(255) PRIMARY KEY,
   user_name               VARCHAR(255),
   client_id               VARCHAR(255)
 );
@@ -50,7 +50,7 @@ CREATE TABLE oauth_client_token (
 CREATE TABLE oauth_access_token (
   token_id                VARCHAR(255),
   token                   bytea,
-  authentication_id       VARCHAR(255),
+  authentication_id       VARCHAR(255) PRIMARY KEY,
   user_name               VARCHAR(255),
   client_id               VARCHAR(255),
   authentication          bytea,
