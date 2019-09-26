411d311cd31b944d7e91740709c2c873d2c83dfb
Daniel Tirzuman <github@tirzuman.com>
2019-09-26 23:23:32 +0300

    fix ecs-cli compose for staging env

--- travis_scripts/deploy_to_amazon_ecs-staging.sh
+++ travis_scripts/deploy_to_amazon_ecs-staging.sh
@@ -2,5 +2,5 @@
 echo "Launching $BUILD_NAME IN AMAZON Elastic Container Service"
 ecs-cli configure profile --profile-name ghf-api --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 ecs-cli configure --region eu-central-1 --cluster staging-gohelpfund-api
-ecs-cli compose --file docker/prod-api/docker-compose.yml --ecs-params docker/staging-api/ecs-params.yml up
+ecs-cli compose --file docker/staging-api/docker-compose.yml --ecs-params docker/staging-api/ecs-params.yml up
 rm -rf ~/.ecs
